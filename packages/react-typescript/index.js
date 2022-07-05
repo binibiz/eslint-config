8eb040b38d559237f0c04f0fe4b5019bea114fae
Bruno Fonseca <brunomacf@gmail.com>
2022-07-05 13:58:20 -0300

    fix(react-typescript): import typescript after react so we can turn eslint rules off

--- packages/react-typescript/index.js
+++ packages/react-typescript/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
-    '@binibiz/eslint-config-typescript',
     '@binibiz/eslint-config-react',
+    '@binibiz/eslint-config-typescript',
     './rules/react-typescript',
   ].map(require.resolve),
   rules: {}
